0fc40e2e7d614afd96b8ffec296fe196c6f4e964
Zulfikar Nore <zulfikarnore@live.com>
2017-08-18 23:16:57 +0100

    Initial Comit
    Initiating the repository with the first comit

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Nore Child
+ * @since 1.0.0
+ */
